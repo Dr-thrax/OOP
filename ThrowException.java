b709142664e36259682e6df58f9fe0a9c07f4020
Tanvir <donlol1845@gmail.com>
2019-03-30 13:04:54 +0600

    Types of Exception

---
+++ ThrowException.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package threaddemo1;
+
+/**
+ *
+ * @author Dr. Thrax
+ */
+public class ThrowException {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws InterruptedException{
+        
+        Thread.sleep(10000); 
+        System.out.println("This is an example of throwing interruptedexception"); 
+    }
+    
+}
